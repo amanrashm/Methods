ac6cab52a75713634b9a1f9de6ce6a7299e8f2ee
amanrashm <amanra664@gmail.com>
2022-05-12 22:06:55 +0530

    Initial commit

---
+++ src/com/aman/Methods.java
@@ -0,0 +1,29 @@
+package com.aman;
+
+import static java.lang.Long.sum;
+
+public class Methods {
+
+    public static int main(String[] args) {
+        // write your code here
+        static void sum(int a , int b){
+            b = 6;
+            a =5;
+        String [] cars = {"maruti","hundai"};
+        try{
+            System.out.println(cars [1]);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+        System.out.println("masoom");
+
+
+
+            System.out.println(sum(a,b));
+            return  a+b;
+        }
+
+    }
+}
